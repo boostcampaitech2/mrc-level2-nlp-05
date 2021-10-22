77cbe9d11e8de83c3811e0fac0e85d197556e6d5
Minjin Kim <kkmjkim1528@gmail.com>
2021-10-22 14:35:30 +0000

    update minjin shell script

--- script/train_mrc_run_minjin.sh
+++ script/train_mrc_run_minjin.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 #rm -rf ./models/*
+
 python train_mrc.py \
-  --run_name exp003_xlm_roberta_large_aware \
-  --output_dir ./models \
-  --model a-ware/xlmroberta-squadv2 \
-  --num_train_epochs 10 \
-  --use_max_padding \
-  --do_train \
-  --do_eval \
-  --evaluation_strategy steps \
-  --eval_steps 500 \
-  --wandb_project mrc-xlm-roberta-large
+--run_name exp004_klue_bert_base \
+--output_dir ./models \
+--model klue/bert-base \
+--num_train_epochs 5 \
+--use_max_padding \
+--eval_steps 500 \
+--warmup_steps 500 \
+--wandb_project mrc-klue-based-models
