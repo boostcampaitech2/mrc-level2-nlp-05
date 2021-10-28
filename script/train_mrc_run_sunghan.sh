f1f1399cade6b31b3f294c62c74b11120947f2ad
zgotter <shkim4738@gmail.com>
2021-10-28 03:24:59 +0000

    add custom head classes

--- script/train_mrc_run_sunghan.sh
+++ script/train_mrc_run_sunghan.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 python train_mrc.py \
-    --run_name exp008_custom_head_rnn_more_dropout_freeze_1_2 \
-    --description "custom head rnn with dropout 0.5, freeze in 1 epoch, modify save method" \
+    --run_name exp008_custom_head_sh_7 \
+    --description "LSTM 3 layer + CNN custom head" \
     --output_dir ./models \
     --model klue/roberta-large \
     --num_train_epochs 5 \
     --use_max_padding \
     --do_eval \
     --warmup_steps 500 \
-    --eval_steps 200 \
+    --eval_steps 100 \
     --wandb_project mrc-head \
     --freeze_pretrained_weight first \
-    --freeze_pretrained_weight_epoch 1 \
-	--head CustomHeadRNN \
-    --head_dropout_ratio 0.5 \
-    --save_total_limit 3
+    --freeze_pretrained_weight_epoch 2 \
+    --save_total_limit 3 \
+    --head CustomHeadLSTM_L3_CNN \
+    --head_dropout_ratio 0.5
