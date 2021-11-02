c04ecb861f4b5a22a70c13380a8704b00496afea
zeus0007 <wpdn79@likelion.org>
2021-11-02 16:58:33 +0000

    docs: script 수정

--- script/inference_zeu.sh
+++ script/inference_zeu.sh
@@ -2,7 +2,7 @@
 
 # exp008
 python inference.py \
---output_dir ./outputs/merge_test \
---model ./models/masking_test1/checkpoint-3200 \
+--output_dir ./outputs/masking_test_roberta_large_0.7 \
+--model ./models/masking_test_roberta_large_0.7/checkpoint-2600 \
 --top_k_retrieval 3 \
 --do_predict
--- script/train_mrc_run_zeu.sh
+++ script/train_mrc_run_zeu.sh
@@ -100,17 +100,66 @@
   #   --freeze_pretrained_weight_epoch 2 \
   #   --custom_model CustomRobertaForQuestionAnsweringWithMultiHeadAttentionHead
 
-  python train_mrc_v2.py \
-      --do_train --do_eval \
-      --output_dir ./models --logging_dir ./logs --seed 42 \
-      --model klue/roberta-large \
-      --num_train_epochs 5 --learning_rate 3.4e-5 --weight_decay 0.015 \
-      --max_seq_len 512 --max_ans_len 30 \
-      --evaluation_strategy steps --eval_steps 100 --logging_steps 100 --save_steps 100 \
-      --save_total_limit 5 \
-      --freeze_type roberta --freeze_epoch 1.0 \
-      --label_smoothing_factor 0.02 \
-      --run_name masking_test_with_normal2 \
-      --wandb_project mrc-context-mask --wandb_entity this-is-real \
-      --token_masking_ratio 0.3 \
-      --token_masking_with_normal_data True
+# head 안돌아가서 v2  주석처리
+  # python train_mrc_v2.py \
+  #     --do_train --do_eval \
+  #     --output_dir ./models --logging_dir ./logs --seed 42 \
+  #     --model klue/roberta-large \
+  #     --num_train_epochs 5 --learning_rate 3.4e-5 --weight_decay 0.015 \
+  #     --max_seq_len 512 --max_ans_len 30 \
+  #     --evaluation_strategy steps --eval_steps 100 --logging_steps 100 --save_steps 100 \
+  #     --save_total_limit 1 \
+  #     --freeze_type roberta --freeze_epoch 1.0 \
+  #     --label_smoothing_factor 0.02 \
+  #     --run_name masking_test_attention_head_v2 \
+  #     --wandb_project mrc-head-test --wandb_entity this-is-real \
+  #     --custom_model CustomRobertaForQuestionAnsweringWithAttentionHead
+
+  # python train_mrc_v2.py \
+  #     --do_train --do_eval \
+  #     --output_dir ./models --logging_dir ./logs --seed 42 \
+  #     --model klue/roberta-large \
+  #     --num_train_epochs 5 --learning_rate 3.4e-5 --weight_decay 0.015 \
+  #     --max_seq_len 512 --max_ans_len 30 \
+  #     --evaluation_strategy steps --eval_steps 100 --logging_steps 100 --save_steps 100 \
+  #     --save_total_limit 1 \
+  #     --freeze_type roberta --freeze_epoch 1.0 \
+  #     --label_smoothing_factor 0.02 \
+  #     --run_name masking_test_attention_cnn_head_v2 \
+  #     --wandb_project mrc-head-test --wandb_entity this-is-real \
+  #     --custom_model CustomRobertaForQuestionAnsweringWithAttentionCNNHead
+
+
+python train_mrc.py \
+    --run_name test_attention_cnn_head_roberta_large_f1 \
+    --description "attention_cnn_head freeze 1" \
+    --output_dir ./models \
+    --logging_dir ./logs --seed 42 \
+    --model klue/roberta-large \
+    --num_train_epochs 5 \
+    --use_max_padding \
+    --do_eval \
+    --warmup_steps 500 \
+    --eval_steps 100 \
+    --wandb_project mrc-head-test \
+    --freeze_pretrained_weight first \
+    --freeze_pretrained_weight_epoch 1 \
+    --save_total_limit 2 \
+    --custom_model CustomRobertaForQuestionAnsweringWithAttentionCNNHead \
+
+python train_mrc.py \
+    --run_name test_attention_head_roberta_large_f1 \
+    --description "attention head freeze1" \
+    --output_dir ./models \
+    --logging_dir ./logs --seed 42 \
+    --model klue/roberta-large \
+    --num_train_epochs 5 \
+    --use_max_padding \
+    --do_eval \
+    --warmup_steps 500 \
+    --eval_steps 100 \
+    --wandb_project mrc-head-test \
+    --freeze_pretrained_weight first \
+    --freeze_pretrained_weight_epoch 1 \
+    --save_total_limit 2 \
+    --custom_model CustomRobertaForQuestionAnsweringWithAttentionHead \
