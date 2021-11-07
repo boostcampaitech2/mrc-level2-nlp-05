ec1ecaca79acfd16f869b120b0342972633bc3a7
shkim4738 <shkim4738@gmail.com>
2021-11-07 13:36:02 +0900

    refactor: inference shell script 수정

--- script/inference_example.sh
+++ script/inference_example.sh
@@ -3,6 +3,6 @@
 python inference.py \
     --output_dir ./outputs/klue_bert_base \
     --dataset_path ../data/test_dataset/ \
-    --model klue/bert-base \
+    --model ./models/exp011_klue_roberta_large_lstm_lm_concat_y \
     --top_k_retrieval 1 \
     --do_predict
